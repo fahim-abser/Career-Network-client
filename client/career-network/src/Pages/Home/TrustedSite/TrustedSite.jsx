b40aabf52da6922ff7d76be237b2dafe1396d461
unknown <nomanmd1st2001@gmail.com>
2023-01-16 23:38:21 +0600

    update full page

--- client/career-network/src/Pages/Home/TrustedSite/TrustedSite.jsx
+++ client/career-network/src/Pages/Home/TrustedSite/TrustedSite.jsx
@@ -3,7 +3,7 @@ import airbrb from '../../../../src/others/TrustedImg/airbrb.png'
 import microsoft from '../../../../src/others/TrustedImg/microsoft.png'
 import google from '../../../../src/others/TrustedImg/google.png'
 import meta from '../../../../src/others/TrustedImg/meta.png'
-import samsung from '../../../../src/others/TrustedImg/samsung.png'
+import samsung from '../../../../src/others/TrustedImg/amazon.png'
 import TrustedSiteCard from './TrustedSiteCard';
 
 const TrustedSite = () => {
@@ -15,10 +15,10 @@ const TrustedSite = () => {
         samsung
     ]
     return (
-        <div className='flex justify-center'>
+        <div className='flex justify-center container mx-auto'>
             <div className='my-5'>
-                <h2 className="text-4xl text-center font-mono mb-6">Trusted our side</h2>
-                <div className='grid grid-cols-5'>
+                <h2 className="text-4xl text-center font-semibold mb-6 font-secondaryFont">Trusted our side</h2>
+                <div className='grid lg:grid-cols-5 md:grid-cols-2 gap-2'>
                     {logos.map(logo => <TrustedSiteCard logo={logo}></TrustedSiteCard>)}
                 </div>
             </div>
--- client/career-network/src/Pages/JobPost/JobPost.jsx
+++ client/career-network/src/Pages/JobPost/JobPost.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const JobPost = () => {
     return (
-        <div className='shadow-2xl rounded-md py-10 font-mono w-11/12 mx-auto'>
-            <h2 className='text-3xl text-center font-bold'>Get it to your job</h2>
-            <p className='text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione <br /> veniam dignissimos officiis magnam vel! Dolorum maiores quos autem dolores qui?</p>
-            <div className='flex justify-center my-5 gap-x-4'>
-                <input className='btn border-none bg-gray-500' type="submit" value="Post A Job" />
-                <input className='btn border-none bg-green-400' type="submit" value="Search" />
+        <div className='shadow-lg shadow-slate-500 rounded-md py-10 w-11/12 mx-auto'>
+            <h2 className='text-4xl text-center font-bold font-secondaryFont'>Get it to your job</h2>
+            <p className='text-center my-4 font-besicFont text-lg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione <br /> veniam dignissimos officiis magnam vel! Dolorum maiores quos autem dolores qui?</p>
+            <div className='flex justify-center gap-x-4'>
+                <input className='btn w-28 border-none bg-gray-500 hover:bg-gray-500' type="submit" value="Post A Job" />
+                <input className='btn w-28 border-none bg-green-400 hover:bg-green-400' type="submit" value="Search" />
             </div>
         </div>
     );
