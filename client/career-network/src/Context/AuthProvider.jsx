2c46c033155e957e9bfea7354fba82d5d9222d39
unknown <abujayed007@gmail.com>
2023-01-17 00:51:09 +0600

    authentication done

---
+++ client/career-network/src/Context/AuthProvider.jsx
@@ -0,0 +1,55 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import app from "../firebase/firebase.config";
+
+export const AuthContext = createContext()
+const auth = getAuth(app)
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+
+    const registerUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const logIn =(email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const logOut = () =>{
+        return signOut(auth);
+    }
+
+    const googleSignIn = (provider) =>{
+        return signInWithPopup(auth, provider)
+    }
+    const facebookSignIn = (provider) =>{
+        return signInWithPopup(auth, provider)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+        });
+
+        return () => unsubscribe();
+    }, [])
+
+    const authInfo = {
+        user,
+        registerUser,
+        logIn,
+        logOut,
+        googleSignIn,
+        facebookSignIn
+
+
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
