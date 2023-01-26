262ac5e66b2e0a34f59d9af80f3e7e73ee8cfdb5
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-01-27 01:05:54 +0600

    error img

--- client/career-network/src/Pages/DisplayError/DisplayError.js
+++ client/career-network/src/Pages/DisplayError/DisplayError.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouteError } from 'react-router-dom';
+import img from '../../others/images/error.jpg'
 
 const DisplayError = () => {
     const error = useRouteError();
@@ -7,6 +8,7 @@ const DisplayError = () => {
         <div className='px-10 bg-zinc-100'>
             <p className='text-red-500 font-bold text-center'>Something went wrong!!!</p>
             <p className='text-red-400 text-center'>{error.statusText || error.message}</p>
+            <img src={img} alt="" />
         </div>
     );
 };
