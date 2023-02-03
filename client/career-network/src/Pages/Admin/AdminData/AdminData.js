8f1b0b82914589f1db44cc4f34ccb261c2185ea3
abdul al noman <nomanmd1st2001@gmail.com>
2023-02-03 20:46:41 +0600

    added

--- client/career-network/src/Pages/Admin/AdminData/AdminData.js
+++ client/career-network/src/Pages/Admin/AdminData/AdminData.js
@@ -7,7 +7,7 @@ function AdminData() {
       <div className='text-center md:w-8/12 w-10/12'>
           <h2 className='text-5xl font-bold capitalize '>admin dashbord direction</h2>
           <p className='my-4'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
-          <div className='gap-3 flex justify-center items-center'>
+          <div className='gap-3  flex justify-center items-center'>
             <Link to={'/admin/recruiter'}><button className='btn btn-primary'>recruite</button></Link>
             <Link to={"/admin/jobseerker"}><button className='btn btn-secondary'>job seeker</button></Link>
             <Link to={"/admin/admintable"}><button className='btn-info btn'>admin</button></Link>
