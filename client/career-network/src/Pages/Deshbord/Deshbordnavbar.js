8c650c4e4e66ae3d55f4f9b2f02aab3f3925ebf3
nhn1998 <nhnayeem1@gmail.com>
2023-02-09 13:58:37 +0600

    git

--- client/career-network/src/Pages/Deshbord/Deshbordnavbar.js
+++ client/career-network/src/Pages/Deshbord/Deshbordnavbar.js
@@ -12,7 +12,7 @@ function Deshbordnavbar() {
     const {user, logOut} = useContext(AuthContext)
     const handleLogOut = () => {
         logOut()
-            .then(() => {})
+            .then(() =>{})
             .catch(err => console.log(err));
     }
 
