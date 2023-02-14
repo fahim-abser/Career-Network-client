b9d3468e36517be4b462f0a32d9805758b3d7490
shaon28 <67582213+SHAON1028@users.noreply.github.com>
2023-02-15 01:22:41 +0600

    seach page fix

--- client/career-network/src/Pages/JobPage/Pagination.jsx
+++ client/career-network/src/Pages/JobPage/Pagination.jsx
@@ -1,39 +1,73 @@
 import React from 'react';
 
-const Pagination = ({ pages, page, setPage }) => {
-    console.log(pages, page)
+const Pagination = ({ totalJobs, postsPerPage, setCurrentPage, setPostsPerPage, currentPage }) => {
+    const pageNum = []
+    for (let i = 1; i <= Math.ceil(totalJobs / postsPerPage); i++) {
+        pageNum.push(i)
+
+    }
     return (
         <div className='mt-20'>
 
 
             <>
-                <div className="flex justify-center">
-                    <button className="flex items-center justify-center px-4 py-2 mx-1 text-gray-500 capitalize bg-white rounded-md cursor-not-allowed rtl:-scale-x-100 dark:bg-gray-800 dark:text-gray-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
-                        </svg>
-                    </button>
 
-                    {/* <button className="hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
-        1
-    </button> */}
+
+                {/* new */}
+                <div class="flex justify-center ">
                     {
-                        [...Array(pages).keys()].map(number => <button key={number} className={page === number ? "hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-blue-500 dark:bg-blue-500 rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200" : "hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-gray-500 dark:hover:bg-gray-500 hover:text-white dark:hover:text-gray-200"}
-                            onClick={() => setPage(number)}
+                        currentPage === 1 ? <button class="px-4 py-2 mx-1 text-gray-500 capitalize bg-white rounded-md cursor-not-allowed dark:bg-gray-800 dark:text-gray-600">
+                            <div class="flex items-center -mx-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-1 rtl:-scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
+                                </svg>
+
+                                <span class="mx-1">
+                                    previous
+                                </span>
+                            </div>
+                        </button>
+                            :
+                            <button onClick={() => setCurrentPage(currentPage - 1)} class="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+                                <div class="flex items-center -mx-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-1 rtl:-scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
+                                    </svg>
+
+                                    <span class="mx-1">
+                                        previous
+                                    </span>
+                                </div>
+                            </button>
+
+                    }
+
+                    {/* numbers */}
+                    {
+                        pageNum.map(number => <button key={number} className={currentPage === number ? " px-4 py-2 mx-1 text-white transition-colors duration-300 transform bg-blue-500 dark:bg-blue-500 rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200" : "hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-gray-500 dark:hover:bg-gray-500 hover:text-white dark:hover:text-gray-200"}
+                            onClick={() => setCurrentPage(number)}
                         >
-                            {number + 1}
+                            {number}
                         </button>)
                     }
-                    {
-                        page + 1 < pages ? <button className="flex items-center justify-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md rtl:-scale-x-100 dark:bg-gray-800 dark:text-gray-200 hover:bg-gray-500 dark:hover:bg-gray-500 hover:text-white dark:hover:text-gray-200" onClick={() => setPage(page + 1)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"
 
-                                />
-                            </svg>
-                        </button> : ""
+                    {/* numbers */}
+                    {
+                        pageNum.length > currentPage && <button onClick={() => setCurrentPage(currentPage + 1)} class="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+                            <div class="flex items-center -mx-1">
+                                <span class="mx-1">
+                                    Next
+                                </span>
+
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-1 rtl:-scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                                </svg>
+                            </div>
+                        </button>
                     }
                 </div>
+
+
             </>
 
 
