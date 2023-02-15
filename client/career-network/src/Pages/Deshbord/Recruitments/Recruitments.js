b9bd23ef17626d0eeb224b45aebb2136cba2b8b8
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-02-15 22:04:57 +0600

    job delete

--- client/career-network/src/Pages/Deshbord/Recruitments/Recruitments.js
+++ client/career-network/src/Pages/Deshbord/Recruitments/Recruitments.js
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react'
+import { toast } from 'react-hot-toast'
 import { useQuery } from 'react-query'
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../../../Context/AuthProvider'
 
 function Recruitments() {
   const { user } = useContext(AuthContext)
-  const { data: postingJobData = [] } = useQuery({
+  const { data: postingJobData = [], refetch } = useQuery({
     queryKey: [],
     queryFn: (async () => {
       const res = await fetch(`http://localhost:5000/recuriterjob?email=${user?.email}`)
@@ -13,6 +14,24 @@ function Recruitments() {
       return data
     })
   })
+
+
+  const handleDeleteJob = job => {
+    fetch(`http://localhost:5000/deletejob/${job._id}`, {
+      method: 'DELETE',
+      headers: {
+        authorization: `bearer ${localStorage.getItem('accessToken')}`
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.deletedCount > 0) {
+          refetch();
+          toast.success(`${job.job_title} deleted successfully`)
+        }
+      })
+  }
+
   console.log(postingJobData)
   return (
     <div className='m-5'>
@@ -25,7 +44,7 @@ function Recruitments() {
               <th>Image</th>
               <th>Name</th>
               <th>Pay</th>
-
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
@@ -40,7 +59,8 @@ function Recruitments() {
                   </div>
                 </td>
                 <td>{job.job_title}</td>
-                <td>{job?.isPaid !== true && <Link to={`/employedashboard/payment/${job._id}`}><button className='btn btn-xs btn-info'>Pay</button></Link>}{job?.isPaid === true && <button className='btn btn-xs  btn-success'>Paid</button>}</td>
+                <td>{job?.isPaid !== true && <Link to={`/employedashboard/payment/${job._id}`}><button className='btn btn-xs btn-info'>Pay For Feature</button></Link>}{job?.isPaid === true && <button className='btn btn-xs  btn-success'>Paid For Featured</button>}</td>
+                <td>{job && <button onClick={() => handleDeleteJob(job)} className='btn btn-xs btn-error'>Delete This Job</button>}</td>
               </tr>)
             }
 
