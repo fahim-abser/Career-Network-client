d03092ced13d44226f93c93a2de785c2604d5df2
abdul al noman <nomanmd1st2001@gmail.com>
2023-02-17 11:16:33 +0600

    added banner

--- client/career-network/src/Pages/Home/Banner/Banner.jsx
+++ client/career-network/src/Pages/Home/Banner/Banner.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import { BsSearch } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
-// import banner from '../../../others/images/banner.jpeg'
+import heroImg from '../../../assets/Herobanner/Hero-Career-Page.webp'
 
 const Banner = () => {
 
     return (
         <div className=" h-screen w-full mx-auto bg-hero-img bg-center bg-no-repeat bg-cover text-white">
-            <div className="grid place-items-center h-full">
-                <div className=" w-8/12 mx-auto">
-                    <h1 className=" font-bold font-besicFont lg:text-7xl md:text-6xl text-5xl">Find the job
+            <div className="grid lg:grid-cols-2 grid-cols-1 place-items-center h-full">
+                <div className=" w-10/12 text-center mx-auto text-slate-800 dark:text-home">
+                    <h1 className=" font-bold lg:text-7xl text-7xl">Find the job
                         that fits your life</h1>
-                    <p className="my-5 md:text-lg capitalize">cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <div className='flex justify-center gap-6'>
-                        <button className="btn w-32 bg-green-400 hover:bg-green-400 border-none disabled:pointer-events-none">Register</button>
-                        <Link to='/contact/email' className="btn w-32 bg-blue-400 hover:bg-blue-400 border-none disabled:pointer-events-none">Contact us</Link>
+                    <p className="my-5 md:text-lg capitalize text-black">Find your dream job with ease. Our job portal connects you with top employers and offers a wide range of opportunities to suit your skills and experience.</p>
+                    <div className='flex justify-center items-center border-2 bg-white rounded-full gap-3'>
+                        <input className='input pl-6 w-full rounded-l-full focus:outline-none text-xl' type="text" placeholder='job title' name="" id="" />
+                        <input className='input h-full w-full rounded-none focus:outline-none text-xl' type="text" placeholder='job categorie' name="" id="" />
+                        <Link to='/contact/email' className="btn w-32 bg-button hover:bg-button border-none disabled:pointer-events-none rounded-full"> <BsSearch className='mr-1'></BsSearch> search</Link>
                     </div>
                 </div>
-
+                <div className='lg:block hidden'>
+                    <img className='w-ful h-full' src={heroImg} alt="" />
+                </div>
             </div>
         </div>
     );
